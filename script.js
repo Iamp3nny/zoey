0a931902118f935c516b5c4c588bd680086da94f
maynard solares <maynardsolares@gmail.com>
2024-10-16 17:07:26 +0800

    header

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("sidenav").style.width = "250px";
+  document.getElementById("ico").style.visibility = "hidden";
+}
+
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+  document.getElementById("ico").style.visibility = "visible";
+}
