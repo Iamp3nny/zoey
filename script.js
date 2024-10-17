e596808b8d7698d39f645406195270d6fe76b2ce
maynard solares <maynardsolares@gmail.com>
2024-10-17 10:11:31 +0800

    side nav

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ function closeNav() {
   document.getElementById("sidenav").style.width = "0";
   document.getElementById("ico").style.visibility = "visible";
 }
+
+var opacity = 0;
+var intervalID = 0;
+window.onload = fadeIn;
+
+function fadeIn() {
+  setInterval(show, 85);
+}
+
+function show() {
+  var body = document.getElementById("body");
+  opacity = Number(window.getComputedStyle(body).getPropertyValue("opacity"));
+  if (opacity < 1) {
+    opacity = opacity + 0.1;
+    body.style.opacity = opacity;
+  } else {
+    clearInterval(intervalID);
+  }
+}
